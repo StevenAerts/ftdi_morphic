508499005cd48efe1028b282c11fbd4b7e2fa46c
StevenAerts <steven.ge.aerts@gmail.com>
2017-12-11 20:38:27 +0100

    Double fifo loopback version

--- vhdl/ftdi_morphic.vhd
+++ vhdl/ftdi_morphic.vhd
@@ -1,36 +1,11 @@
--------------------------------------------------------------------------------
--- Title      : MorphIC 2 v1.2 wrapper using synchronous 245 Interface for FT2232H
--------------------------------------------------------------------------------
--- File       : MorphIC_HS_245_Sync_Fifo.vhd
--- Author     : AJ DOUGAN 
--- Company    : Future Technology Devices International
--- Created    : 2009-10-29
--- Last update: 2009-10-29
--- Platform   : 
--- Standard   : VHDL'93
--------------------------------------------------------------------------------
--- Description: synchronous 245 interface between FT2232H and FT2232H device.
---              Data will be transferred in one direction at a time between the
---              two devices. If full duplex operation is required then arbitration
---              logic should be added at this level.
--------------------------------------------------------------------------------
--- Copyright (c) 2009 
--------------------------------------------------------------------------------
--- Revisions  :
--- Date        Version  Author  Description
--- 2007-10-28  1.0      AJD     Created
--------------------------------------------------------------------------------
-
-
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity ftdi_morphic is
    port (-- Inputs
-					-- clk50     : in  std_logic;       -- 50MHz clock input unused
+	  clk50     : in  std_logic;       -- 50MHz clock input unused
       rst       : in  std_logic;       -- Active high reset via BD7
 
--- Morphic on board FT2232H signals
       mdata     : inout std_logic_vector(7 downto 0);   -- Port A Data Bus
       mclk60    : in std_logic;
       mrxfn     : in std_logic;
@@ -38,7 +13,8 @@ entity ftdi_morphic is
       mrdn      : out std_logic;
       mwrn      : out std_logic;
       moen      : out std_logic;
-		msndimm   : out std_logic
+	  msndimm   : out std_logic;
+	  io		: inout std_logic_vector(79 downto 0)
 );
    end ftdi_morphic;
 
@@ -84,130 +60,85 @@ component hs245_sif
     );
 end component;
 
-component sync_pll
-	PORT
-	(
-		inclk0		: IN STD_LOGIC  := '0';
-		c0		: OUT STD_LOGIC ;
-		locked		: OUT STD_LOGIC 
-	);
-end component;
-
-
-component seq_trig 
-  port (
---------------------
--- Inputs
---------------------
-    clk                 : in std_logic; 
-    reset_n             : in std_logic;
-    sync_reset          : in std_logic;
-    strobe              : in std_logic;
-    mon_bus             : in std_logic_vector(7 downto 0);
-
---------------------
--- Outputs
---------------------
-    trigger             : out std_logic
-  );
-  
-  end component;
-
-  --
-  --  
-
   signal mrxf,mtxe,moe,mrd,mwr : std_logic;
-  signal mdatain  : std_logic_vector(7 downto 0);
-  signal mdataout : std_logic_vector(7 downto 0);
-  signal mdataen  : std_logic;
+  signal mdatain, mdataout : std_logic_vector(7 downto 0);
   
   signal s1_wr,s1_txe,s1_rd,s1_rxf,s1_mrd : std_logic;
+  signal s2_wr,s2_txe,s2_rd,s2_rxf,s2_mrd : std_logic;
 
   signal reset_n : std_logic;
 
---  signal hrxf,htxe,hoe,hrd,hwr : std_logic;
---  signal hdatain  : std_logic_vector(7 downto 0);
---  signal hdataout : std_logic_vector(7 downto 0);
---  signal hdataen  : std_logic;
-  
---  signal s2_wr,s2_txe,s2_rd,s2_rxf,s2_hrd : std_logic;
+  signal int_mdatain, int_mdataout : std_logic_vector(7 downto 0);
+  signal int_adatain, int_adataout : std_logic_vector(7 downto 0);
 
---  signal hclk60_pll,pll2_lock : std_logic;
-
---  signal int_hdatain  : std_logic_vector(7 downto 0);
---  signal int_hdataout : std_logic_vector(7 downto 0);
-  signal int_mdatain  : std_logic_vector(7 downto 0);
-  signal int_mdataout : std_logic_vector(7 downto 0);
-
-
---  signal tie_low: std_logic;
---  signal strobe_mor,strobe_hs : std_logic;
-  
+  signal io_in, io_out, io_oe: std_logic_vector(79 downto 0);
 
 begin
+
+io_in <= io;
+io_out(79 downto 1) <= (others =>'0');
+io_oe <= (0=>'1', others => '0');
+io_gen:	for i in io'range generate
+	io(i) <= io_out(i) when io_oe(i) = '1' else 'Z';
+end generate io_gen;
   
---tie_low <= '0';
+mdata_in:	mdatain <= mdata;
+mdata_out: 	mdata <= mdataout when moe='0' else (others => 'Z');
+reset_inv:	reset_n <= not rst; -- polarity to programme over USB
+mrxf_inv:	mrxf <= not mrxfn;
+mtxe_inv:	mtxe <= not mtxen;
+moe_inv:	moen <= not moe;
+mrd_inv:	mrdn <= not mrd;
+mwr_inv:	mwrn <= not mwr;
+msndimm_o:	msndimm <= '1';
 
---pll1_mor : sync_pll
---	port map
---	(
---		inclk0	=> mclk60,
---		c0		=> mclk60_pll,
---		locked	=> pll1_lock
---	);
-
---========================================================
---== Create buffers for all Bidi I/Os for Morphic FT2232H side
---========================================================
-
-mdataen <= not moe;
-
-GEN_MORPHIC_DATABUS:
-
-for I in 0 to 7 generate
-
-mdbus_I : process(mdataout,mdataen)
+dummy_proc: process(clk50)
 begin
-if (mdataen='1') then
-   mdata(I) <= mdataout(I);
-else
-   mdata(I) <= 'Z';
-end if;
-end process mdbus_I;
-
-mdatain(i) <= mdata(i);
-
-end generate GEN_MORPHIC_DATABUS;
-
---reset_n <= rst; -- polarity to programme over JTAG
-reset_n <= not rst; -- polarity to programme over USB
+  if rising_edge(clk50) then
+	--if rst = '1' then
+	io_out(0) <= io_in(1);
+  end if;
+end process dummy_proc;
 
 sync1 : sync_fifo 
    port map(                                 
       reset_n  => reset_n,
---
+
       s_clk    => mclk60,
       s_wr     => s1_wr,
       s_txe    => s1_txe,
       s_dbin   => int_mdatain,
---
---      d_clk    => hclk60_pll,
+
       d_clk    => mclk60,
       d_rd     => s1_rd,
       d_rxf    => s1_rxf,
+      d_dbout  => int_adatain
+   );
+
+sync2 : sync_fifo 
+   port map(                                 
+      reset_n  => reset_n,
+
+      s_clk    => mclk60,
+      s_wr     => s2_wr,
+      s_txe    => s2_txe,
+      s_dbin   => int_adataout,
+
+      d_clk    => mclk60,
+      d_rd     => s2_rd,
+      d_rxf    => s2_rxf,
       d_dbout  => int_mdataout
    );
 
-
-mrxf <= not mrxfn;
-mtxe <= not mtxen;
-
+int_adataout <= int_adatain;
+s2_wr <= s1_rd ;
+s1_rd <= s1_rxf and s2_txe;
 
 xfer1 : hs245_sif 
   port map(                                 
     clk        => mclk60,
     reset_n    => reset_n,
---
+
     ext_txe    => mtxe,
     ext_rxf    => mrxf,
     ext_wr     => mwr,
@@ -215,95 +146,17 @@ xfer1 : hs245_sif
     ext_oe     => moe,
     ext_datain => mdatain,
     ext_dataout => mdataout,
---
+
     int_datain  => int_mdataout,
-    int_rxf     => s1_rxf,
-    int_rd      => s1_rd,
---
+    int_rxf     => s2_rxf,
+    int_rd      => s2_rd,
     int_dataout => int_mdatain,
     int_txe     => s1_txe,
     int_wr      => s1_wr
-
-    );
-
-
---sync2 : sync_fifo 
---   port map(                                 
---      reset_n  => reset_n,
-
---      s_clk    => hclk60,
---      s_wr     => s2_wr,
---      s_txe    => s2_txe,
---      s_dbin   => int_hdatain,
---
---      d_clk    => hclk60,
---      d_rd     => s2_rd,
---      d_rxf    => s2_rxf,
---      d_dbout  => int_hdataout
---   );
+ );
 
 
 
---hrxf <= not hrxfn;
---htxe <= not htxen;
-
---xfer2 : hs245_sif 
---  port map(                                 
-
---    clk        => hclk60,
---    reset_n    => reset_n,
-
---    ext_txe    => htxe,
---    ext_rxf    => hrxf,
---    ext_wr     => hwr,
---    ext_rd     => hrd,
---    ext_oe     => hoe,
---    ext_datain => hdatain,
---    ext_dataout => hdataout,
-
---    int_datain  => int_hdataout,
---    int_rxf     => s2_rxf,
---    int_rd      => s2_rd,
-
---    int_dataout => int_hdatain,
---    int_txe     => s2_txe,
---    int_wr      => s2_wr
-
---    );
-
-
-moen <= not moe;
-mrdn <= not mrd;
-mwrn <= not mwr;
-
-msndimm <= '1';
-
-
---hoen <= not hoe;
---hrdn <= not hrd;
---hwrn <= not hwr;
-
---hsndimm <= '1';
-
---------------------------------------------------------------------------------
--- Create PLL
---------------------------------------------------------------------------------
-
---pll2_hs : sync_pll
---	port map
---	(
---		inclk0	=>	hclk60,
---		c0		=> hclk60_pll,
---		locked	=> pll2_lock
---	);
-
-
---========================================================
---== Create buffers for all Bidi I/Os for External data bus
---========================================================
-
-
---hdataen <= not hoe;
 
 end rtl;
 
--- vhdl/testbench.vhd
+++ vhdl/testbench.vhd
@@ -1,33 +1,32 @@
-
 library std;
 use std.textio.all;           
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_textio.all;
 use ieee.numeric_std.all;
---use ieee.std_logic_arith.all;
  
 entity fifo_stim is
-    generic(
-        clockperiod : time := 20 ns;
-        id : string := "fifo"
-    );
-    port(
-      rst      : in  std_logic;
-      data     : inout std_logic_vector(7 downto 0);
-      clk      : out std_logic;
-      rxfn     : out std_logic;
-      txen     : out std_logic;
-      rdn      : in  std_logic;
-      wrn      : in  std_logic;
-      oen      : in  std_logic
-    );
+  generic(
+	clockperiod		: time := 17 ns;
+    id 				: string := "fifo"
+  );
+  port(
+	rst      		: in  	std_logic;
+	data     		: inout std_logic_vector(7 downto 0);
+	clk      		: out 	std_logic;
+	rxfn     		: out 	std_logic;
+	txen     		: out 	std_logic;
+	rdn      		: in  	std_logic;
+	wrn      		: in  	std_logic;
+	oen      		: in  	std_logic
+  );
 end fifo_stim;
 
 architecture proc of fifo_stim is
 
-signal clki : std_logic := '0';
-signal datai  : std_logic_vector(7 downto 0) := "00000001";
+signal clki 		: std_logic := '0';
+signal datai  		: std_logic_vector(7 downto 0) := "00000001";
 
 begin
 
@@ -66,7 +65,6 @@ procedure readbytes(bytes: out integer_vector) is
 begin
     txen <= '0';
 	for ibyte in bytes'range loop
-        --datai <= std_logic_vector(to_unsigned(bytes(ibyte), datai'length));
         wait until rising_edge(clk);
         while wrn /= '0' loop
             wait until rising_edge(clk);
@@ -85,7 +83,6 @@ variable bytes: integer_vector(3 downto 0) := (0,1,2,3);
 begin
     datai <= (others => '0'); rxfn <= '1'; txen <= '1';
 
-    --bytes := (0, 1, 2, 3);
     writebytes(bytes);
     readbytes(bytes);
     wait;
@@ -103,51 +100,49 @@ end testbench;
 architecture beh of testbench is
 
 signal rst: std_logic;
+signal clk50: std_logic := '0';
 signal mdata,mdatai: std_logic_vector(7 downto 0);
---signal hdata,hdatai  : std_logic_vector(7 downto 0);
 signal mclk60,mrxfn,mtxen,mrdn,mwrn,moen,msndimm : std_logic;
---signal hclk60,hrxfn,htxen,hrdn,hwrn,hoen,hsndimm : std_logic;
-
 
 component ftdi_morphic 
-   port (-- Inputs
-					-- clk50     : in  std_logic;       -- 50MHz clock input unused
-      rst       : in  std_logic;       -- Active high reset via BD7
+  port (
+	clk50     		: in  std_logic;       -- 50MHz clock input unused
+	rst       		: in  std_logic;       -- Active high reset via BD7
 
--- Morphic on board FT2232H signals
-      mdata     : inout std_logic_vector(7 downto 0);   -- Port A Data Bus
-      mclk60    : in std_logic;
-      mrxfn     : in std_logic;
-      mtxen     : in std_logic;
-      mrdn      : out std_logic;
-      mwrn      : out std_logic;
-      moen      : out std_logic;
-		msndimm   : out std_logic           -- unused
-      
-);
+	mdata     		: inout std_logic_vector(7 downto 0);   -- Port A Data Bus
+	mclk60    		: in std_logic;
+	mrxfn     		: in std_logic;
+	mtxen     		: in std_logic;
+	mrdn      		: out std_logic;
+	mwrn      : out std_logic;
+	moen      : out std_logic;
+	msndimm   : out std_logic;
+	io				: inout std_logic_vector(79 downto 0)
+  );
 end component;
 
 component fifo_stim is
-    generic(
-        clockperiod : time := 20 ns;
-        id : string := "fifo"
-    );
-    port(
-      rst      : in  std_logic;
-      data     : inout std_logic_vector(7 downto 0);
-      clk      : out std_logic;
-      rxfn     : out std_logic;
-      txen     : out std_logic;
-      rdn      : in  std_logic;
-      wrn      : in  std_logic;
-      oen      : in  std_logic
-    );
+  generic(
+	clockperiod : time := 17 ns;
+	id : string := "fifo"
+  );
+  port(
+	rst      : in  std_logic;
+	data     : inout std_logic_vector(7 downto 0);
+	clk      : out std_logic;
+	rxfn     : out std_logic;
+	txen     : out std_logic;
+	rdn      : in  std_logic;
+	wrn      : in  std_logic;
+	oen      : in  std_logic
+  );
 end component;
 
 
 begin
 i_fifo : ftdi_morphic 
    port map(                                 
+      clk50=>clk50, 
       rst=>rst, 
       mdata=>mdata,
       mclk60=>mclk60,
@@ -161,7 +156,7 @@ i_fifo : ftdi_morphic
 
 i_mfifo_stim: fifo_stim
     generic map (
-        clockperiod => 20 ns,
+        clockperiod => 17 ns,
         id => "mfifo"
     )
     port map (
@@ -183,6 +178,12 @@ begin
     rst <= '0';
     wait;
 end process rst_proc;
+
+clk50_proc: process
+begin
+    wait for 10 ns;
+    clk50 <= not clk50;
+end process clk50_proc;
 
 end beh;
 
